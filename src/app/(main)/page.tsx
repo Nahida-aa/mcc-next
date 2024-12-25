2376411c79fe60a2c21b1a3cc084fc8d63165968
aa <Nahida-aa@outlook.com>
2024-12-25 23:45:58 +0800

    doc

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -12,13 +12,30 @@ export default async function AA() {
     redirect('https://api.Nahida-aa.us.kg')
   }
   return (
-    <main >
+    <main className='space-y-2'>
       {/* <HomeHeader user={session?.user} /> */}
-      <a href="https://api.Nahida-aa.us.kg" target="_blank">
-        <Button >
-          去 api 交互文档(FastApi)
-        </Button>
+      <Button className='h-auto'>
+        <a href="/api/hono" target="_blank">
+          去 api 交互文档(Scalar UI)
+        </a>
+      </Button><br />
+      <Button className='h-auto'>
+        <a href="/docs" target="_blank">
+          去 api 交互文档(Swagger UI)<br />
+          样式有问题, 我没完整的进行修复, 不建议使用, <br />
+          因为我有提供 openapi.json 可以下载或复制源代码
+        </a>
+      </Button><br />
+      <Button >
+      <a href="/api/hono/doc" target="_blank">
+          去 openapi.json 
       </a>
+      </Button><br />
+      <Button>
+        <a href="https://api.Nahida-aa.us.kg" target="_blank" >
+          去 api 交互文档(FastApi)(已废弃)
+        </a>
+      </Button><br />
     </main>
   )
 }
